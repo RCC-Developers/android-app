923857133b374aaa250c5be3780c668c1cb0d7d9
nomad-dev <73686008+david-Tich@users.noreply.github.com>
2022-07-12 22:34:49 +0300

    Update map_key.dart

--- lib/global/map_key.dart
+++ lib/global/map_key.dart
@@ -1,5 +1,3 @@
 //String mapKey = "AIzaSyAgAHadiXN5BFmcmN0dE-WxqDeBtef-c0g";
 
-//String mapKey = "AIzaSyDLBSaBz2MJE-mllHiCPD8DJ8hPGvYFf6A";
-
-String mapKey = "AIzaSyABFuZmbNuyA-XDm9mviUOdaTb-G4iSzUE";
+String mapKey = "AIzaSyDLBSaBz2MJE-mllHiCPD8DJ8hPGvYFf6A";
